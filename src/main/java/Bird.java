8ba6295cd83cbdfadbd5c39784aa62881f3b371b
Khusbu Thapa <khusbuuthapa@gmail.com>
2023-03-03 04:33:45 +0000

    Completed

--- src/main/java/Bird.java
+++ src/main/java/Bird.java
@@ -45,7 +45,16 @@ public class Bird {
      *         greater than or equal to 3.
      */
     public String sound(){
-        return "";
+     //return  (ageMonths < 3) ? "Tweet" :(ageMonths >= 3) ? "Chirp": "No sound";
+         if(ageMonths < 3){
+            return "Tweet";
+         }   else if(ageMonths >= 3){
+            return "Chirp";
+         }else{
+            return "No sound";
+         }
+        
     }
+
 
 }
